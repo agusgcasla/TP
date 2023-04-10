3d167dd1419f781383d8d9642d8fefddc5aeb922
Agus <agus@Mini-A.local>
2023-04-10 15:45:31 -0300

    Initial commit

---
+++ src/main/java/Tarea.java
@@ -0,0 +1,46 @@
+import java.time.*;
+
+
+public class Tarea {
+    private String nombre;
+    private String descripcion;
+    private LocalDate fecha;
+    private LocalTime limite;
+    private boolean completada;
+
+    public Tarea (String nombre, String descripcion, LocalDate fecha, LocalTime limite) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.completada = false;
+        this.fecha = fecha;
+        this.limite = limite;
+    }
+
+    public Tarea (String nombre, String descripcion, LocalDate fecha) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.completada = false;
+        this.fecha = fecha;
+        this.limite = LocalTime.MAX;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getLimite() {
+        return limite;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+}
---
+++ src/test/java/TareaTest.java
@@ -0,0 +1,41 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.chrono.ChronoLocalDate;
+
+import static org.junit.Assert.*;
+
+public class TareaTest {
+    @Test
+    public void TareaVacia(){
+        LocalDate d = LocalDate.of(2022, 11, 18);
+        var t = new Tarea("", "", d);
+        Assert.assertEquals("", t.getNombre());
+        Assert.assertEquals("", t.getDescripcion());
+        Assert.assertEquals(d, t.getFecha());
+        Assert.assertEquals(LocalTime.MAX, t.getLimite());
+        Assert.assertFalse(t.isCompletada());
+    }
+    @Test
+    public void TareaConDescripcionYNombre(){
+        LocalDate d = LocalDate.of(2022, 11, 18);
+        var t = new Tarea("Supermercado", "Comprar pan y leche", d);
+        Assert.assertEquals("Supermercado", t.getNombre());
+        Assert.assertEquals("Comprar pan y leche", t.getDescripcion());
+    }
+
+    @Test
+    public void TareaConHoraLimite(){
+        LocalDate fecha = LocalDate.of(2022, 11, 18);
+        LocalTime hora = LocalTime.of(13, 8);
+        var t = new Tarea("Supermercado", "Comprar pan y leche", fecha, hora);
+        Assert.assertEquals(hora, t.getLimite());
+    }
+
+    @Test
+
+
+
+}
